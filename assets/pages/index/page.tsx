f048b20edf2444258907b761e959a22711d31285
Jomar Jay Corona <arrcorona@gmail.com>
2026-03-27 19:16:58 +0800

    🐛

--- assets/pages/index/page.tsx
+++ assets/pages/index/page.tsx
@@ -52,38 +52,41 @@ const IndexPage = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item dropdown">
-                                <a
-                                    className="nav-link dropdown-toggle d-flex align-items-center"
-                                    href="#"
-                                    role="button"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                >
-                                    <i className="bi bi-person-circle me-2"></i> {/* icon */}
-                                    {localStorage.getItem("user")
-                                        ? JSON.parse(localStorage.getItem("user")!).email
-                                        : "Guest"}
-                                </a>
-                                <ul className="dropdown-menu dropdown-menu-dark dropdown-menu-end">
-                                    <li>
-                                        <button
-                                            className="dropdown-item"
-                                            onClick={() => {
-                                                localStorage.removeItem("jwt");
-                                                localStorage.removeItem("user");
-                                                window.location.reload(); // simple logout
-                                            }}
-                                        >
-                                            <i className="bi bi-box-arrow-right me-2"></i> Logout
-                                        </button>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
+                    {localStorage.getItem("user") && (
+                        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item dropdown">
+                                    <a
+                                        className="nav-link dropdown-toggle d-flex align-items-center"
+                                        href="#"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        <i className="bi bi-person-circle me-2"></i>
+                                        {localStorage.getItem("user")
+                                            ? JSON.parse(localStorage.getItem("user")!).email
+                                            : "Guest"}
+                                    </a>
+
+                                    <ul className="dropdown-menu dropdown-menu-dark dropdown-menu-end">
+                                        <li>
+                                            <button
+                                                className="dropdown-item"
+                                                onClick={() => {
+                                                    localStorage.removeItem("jwt");
+                                                    localStorage.removeItem("user");
+                                                    window.location.reload();
+                                                }}
+                                            >
+                                                <i className="bi bi-box-arrow-right me-2"></i> Logout
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </nav>
 
